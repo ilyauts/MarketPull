1a87d594c38e74d9101fc190add40f8be33392ea
ILYA UTS <ilya.uts@jpmorgan.com>
2016-12-05 12:21:59 -0500

    Accounts for splitting

--- marketPull.py
+++ marketPull.py
@@ -8,23 +8,40 @@ closingPrices = []
 lowPrices = []
 highPrices = []
 
+# Indices
+dateIndex = 0
+openingIndex = 1
+closingIndex = 4
+lowIndex = 3
+highIndex = 2
+splitIndex = 7
+
+# Get json file with ticker data
 def pullJSON(ticker):
     url = "https://www.quandl.com/api/v3/datasets/WIKI/" + ticker + ".json"
     response = urllib.urlopen(url)
     data = json.loads(response.read())
     return data
 
-# organize the json into nice date-time maps
+# Organize the json into nice date-time maps
 def parseJSON(data):
     prices = data['dataset']['data'];
+
+    splitMultiplier = 1
+
+    # Adjust for splits
     for i in range(0, len(prices)):
         currPrices = prices[i]
-        dates.append(datetime.strptime(currPrices[0], '%Y-%m-%d'))
-        openingPrices.append(currPrices[1])
-        closingPrices.append(currPrices[4])
-        lowPrices.append(currPrices[3])
-        highPrices.append(currPrices[2])
 
+        dates.append(datetime.strptime(currPrices[dateIndex], '%Y-%m-%d'))
+        openingPrices.append(currPrices[openingIndex] / splitMultiplier)
+        closingPrices.append(currPrices[closingIndex] / splitMultiplier)
+        lowPrices.append(currPrices[lowIndex] / splitMultiplier)
+        highPrices.append(currPrices[highIndex] / splitMultiplier)
+
+        splitMultiplier = splitMultiplier * currPrices[splitIndex]
+
+# Getters
 def getDates():
     return dates
 
@@ -50,4 +67,4 @@ data = pullJSON(ticker)
 parseJSON(data)
 
 # Plotting
-plotPrices(getDates(), getClosingPrices())
+plotPrices(getDates(), getHighPrices())
